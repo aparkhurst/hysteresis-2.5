64f91f20e2484fb21444d17988d390923b5e9e5b
Spencer Maynes <smaynes89@gmail.com>
2013-11-12 12:40:52 -0600

    Convergence improvement to geometric

--- R/geom_ellipse.R
+++ R/geom_ellipse.R
@@ -8,13 +8,19 @@ geom_ellipse <- function(model,control) {
   SSE <- crossprod(x_resid) + crossprod(y_resid)
   SSEratio <- 2
   i=0
-  while (SSEratio > control | SSEratio < 1) {
+  while (SSEratio > control) {
     iter.step <- geom_step(pars,x_resid,y_resid,n)
-    pars <- iter.step[[1]]
-    x_resid <- x-pars[n+4]-pars[n+2]*cos(pars[n+1])*cos(pars[1:length(x)])+pars[n+3]*sin(pars[n+1])*sin(pars[1:length(x)])
-    y_resid <- y-pars[n+5]-pars[n+2]*sin(pars[n+1])*cos(pars[1:length(x)])-pars[n+3]*cos(pars[n+1])*sin(pars[1:length(x)])
+    pars2 <- iter.step[[1]]
+    x_resid <- x-pars2[n+4]-pars2[n+2]*cos(pars2[n+1])*cos(pars2[1:length(x)])+pars2[n+3]*sin(pars2[n+1])*sin(pars2[1:length(x)])
+    y_resid <- y-pars2[n+5]-pars2[n+2]*sin(pars2[n+1])*cos(pars2[1:length(x)])-pars2[n+3]*cos(pars2[n+1])*sin(pars2[1:length(x)])
     SSEold <- SSE
     SSE <- crossprod(x_resid) + crossprod(y_resid)
+    while (SSE > SSEold) {
+      pars2 <- (pars + pars2)/2
+      x_resid <- x-pars2[n+4]-pars2[n+2]*cos(pars2[n+1])*cos(pars2[1:length(x)])+pars2[n+3]*sin(pars2[n+1])*sin(pars2[1:length(x)])
+      y_resid <- y-pars2[n+5]-pars2[n+2]*sin(pars2[n+1])*cos(pars2[1:length(x)])-pars2[n+3]*cos(pars2[n+1])*sin(pars2[1:length(x)])
+      SSE <- crossprod(x_resid) + crossprod(y_resid)
+    }
     SSEratio <- SSEold/SSE
     i=i+1
   }
