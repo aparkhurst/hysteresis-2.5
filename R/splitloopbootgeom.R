279488880ea2c832fae14bb939843306d87f9ac7
spencerm89 <smaynes89@gmail.com>
2014-04-11 13:07:07 -0500

    Fix

--- R/splitloopbootgeom.R
+++ R/splitloopbootgeom.R
@@ -29,10 +29,20 @@ splitloopbootgeom <-
       }
     }
     
-    midspread <- 2*pi/length(x)
-    mod <- optim(par=c("t"=rep(2*pi/length(x),length(x)),"cx"=0,"cy"=0,"b.x"=diff(range(x))/2,"b.y"=diff(range(y))/2,"logm"=0,
-                       "logn"=0,"retention.above"=0,"retention.below"=0),fn=floopCauchyLoss2r,x=x,y=y,
-                 midspread=midspread,method="BFGS",hessian=TRUE)
+ start <- direct(x[1:length(y)],y) 
+
+ti<-numeric(length(y))
+for (i in 1:length(y)) {
+  x0<-x[i]
+  y0<-y[i]
+  zmin1<-optimize(ellipsespot,c(0,pi),"x0"=x0,"y0"=y0,"cx"=start$vals["cx"],"cy"=start$vals["cy"],"semi.major"=start$vals["semi.major"],"semi.minor"=start$vals["semi.minor"],"rote.rad"=start$vals["theta"])
+  zmin2<-optimize(ellipsespot,c(pi,2*pi),"x0"=x0,"y0"=y0,"cx"=start$vals["cx"],"cy"=start$vals["cy"],"semi.major"=start$vals["semi.major"],"semi.minor"=start$vals["semi.minor"],"rote.rad"=start$vals["theta"])
+  ti[i]<-ifelse(zmin1$objective < zmin2$objective, zmin1, zmin2)[[1]]
+}
+inti <- internal.1(start$vals["semi.major"],start$vals["semi.minor"],start$vals["theta"])
+   mod=optim(par=c("t"=c(ti,0,0,0),"cx"=start$vals["cx"],"cy"=start$vals["cy"],"b.x"=inti[1],"b.y"=inti[2],"logm"=0,
+                   "logn"=0,"retention.above"=inti[3],"retention.below"=inti[3]),fn=floopCauchyLoss,x=x,y=y,
+             method="BFGS",hessian=TRUE)
     par <- mod$par
     times <- par[1:length(x)]
     cx <- par[length(x)+1]
