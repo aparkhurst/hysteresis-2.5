2d0c73abee8e04965d71a4667beb94a053d4fe58
spencerm89 <smaynes89@gmail.com>
2014-04-25 14:49:01 -0500

    Update and rename plot.splitloop.R to plot.loop2r.R

--- R/plot.loop2r.R
+++ R/plot.loop2r.R
@@ -1,4 +1,4 @@
-plot.splitloop <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FALSE,main=NULL,...) {
+plot.loop2r <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FALSE,main=NULL,...) {
   a <- x
   ti <- (1:101)*pi/50
   if (a$method=="harmonic2") {
