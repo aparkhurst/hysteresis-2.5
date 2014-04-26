280b3b9926de3f8f4dfca73413b6e67e1552cd20
spencerm89 <smaynes89@gmail.com>
2014-04-25 19:30:40 -0500

    Update plot.loop2r.R

--- R/plot.loop2r.R
+++ R/plot.loop2r.R
@@ -1,4 +1,4 @@
-plot.loop2r <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FALSE,main=NULL,...) {
+plot.loop2r <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FALSE,main=NULL,values=NULL,...) {
   a <- x
   ti <- (1:101)*pi/50
   if (a$method=="harmonic2") {
@@ -18,7 +18,35 @@ plot.loop2r <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FALSE,m
   }
   if (is.null(xlim)) xlim <-c(min(c(a$x,Input)),max(c(a$x,Input)))
   if (is.null(ylim)) ylim <- c(min(c(a$y,Output)),max(c(a$y,Output)))                           
- plot(Output~Input,type="l",ylim=ylim,xlim=xlim,main=main,...)
+if (is.null(values)) plot(Output~Input,type="l",ylim=ylim,xlim=xlim,main=main,...)
+  else {
+    if (values=="inherent") {
+      plot(Output~Input,type="l",ylim=ylim,xlim=xlim,...)
+      title(line=3, paste(main),cex=1.2)
+      mtext(paste(
+        "b.x=",format(a$values["b.x"],digits=3)," b.y=",format(a$values["b.y"],digits=3)),side=3,line=1.85,cex=0.75)
+      mtext(paste("cx=",format(a$values["cx"],digits=3)," cy=",format(a$values["cy"],digits=3)),side=3,line=0.95,cex=0.75)
+      mtext(paste("Retention.above=",format(a$values["retention.above"],digits=3)," Retention.below=",format(a$values["retention.below"],digits=3)),side=3,line=0.0,cex=0.75)
+    }
+    
+    if (values=="hysteresis" | values=="hysteresis.all") {
+      plot(Output~Input,type="l",ylim=ylim,xlim=xlim,...)
+      title(line=3, paste(main),cex=1.2)
+      mtext(paste(
+        "b.x=",format(a$values["b.x"],digits=3)," b.y=",format(a$values["b.y"],digits=3)," cx=",format(a$values["cx"],digits=3)," cy=",format(a$values["cy"],digits=3)),side=3,line=1.85,cex=0.75)
+      mtext(paste("Area=",format(a$values["area"],digits=3)," Lag above=",format(a$values["lag.above"],digits=3)," Lag below=",format(a$values["lag.below"],digits=3)),side=3,line=0.95,cex=0.75)
+      mtext(paste("Retention.above=",format(a$values["retention.above"],digits=3)," Retention.below=",format(a$values["retention.below"],digits=3)," Coercion above=",format(a$values["coercion.above"],digits=3)," Coercion below=",format(a$values["coercion.below"],digits=3)),side=3,line=0.0,cex=0.75)
+    }
+  
+  if (values=="derived") {
+    plot(Output~Input,type="l",ylim=ylim,xlim=xlim,...)
+    title(line=3, paste(main),cex=1.2)
+    mtext(paste(
+      "Coercion above=",format(a$values["coercion.above"],digits=3)," Coercion below=",format(a$values["coercion.below"],digits=3)," Area=",format(a$values["area"],digits=3)),side=3,line=1.85,cex=0.75)
+    mtext(paste(" Lag above=",format(a$values["lag.above"],digits=3)," Lag below=",format(a$values["lag.below"],digits=3)," Split Angle=",format(a$values["split.angle"],digits=3)),side=3,line=0.95,cex=0.75)
+    mtext(paste("Hysteresis x above=",format(a$values["hysteresis.x.above"],digits=3),"Hysteresis x below=",format(a$values["hysteresis.x.below"],digits=3)," Hysteresis y above=",format(a$values["hysteresis.y.above"],digits=3),"Hysteresis y below=",format(a$values["hysteresis.y.below"],digits=3)),side=3,line=0.0,cex=0.75)
+  }
+  }
  
   points(a$y~a$x,pch=1,cex=0.85)
   if (split.line==TRUE) {
