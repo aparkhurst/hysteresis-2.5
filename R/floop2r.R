acad8425c3068068965e687db5f1001aacf34e2b
spencerm89 <smaynes89@gmail.com>
2014-04-26 14:35:19 -0500

    Update floop2r.R

--- R/floop2r.R
+++ R/floop2r.R
@@ -90,7 +90,7 @@ ti<-c(ti[1],diff(ti))
 }
 else ti<-c(t[1],diff(t))
 inti <- internal.1(start$vals["semi.major"],start$vals["semi.minor"],start$vals["theta"])
-   mod=optim(par=c("t"=ti,"cx"=start$vals["cx"],"cy"=start$vals["cy"],"b.x"=(inti[1]+diff(range(dat$x))/2)/2,"b.y"=(inti[2]+diff(range(dat$y))/2)/2,"logm"=log(m),
+   mod=optim(par=c("t"=ti,"cx"=(start$vals["cx"]+mean(dat$x))/2,"cy"=(start$vals["cy"]+mean(dat$y))/2,"b.x"=(inti[1]+diff(range(dat$x))/2)/2,"b.y"=(inti[2]+diff(range(dat$y))/2)/2,"logm"=log(m),
                    "logn"=log(n),"retention.above"=inti[3]/2,"retention.below"=inti[3]/2),fn=floopCauchyLoss,x=dat$x,y=dat$y,
              method="BFGS",hessian=TRUE)
    par <- as.vector(mod$par)
