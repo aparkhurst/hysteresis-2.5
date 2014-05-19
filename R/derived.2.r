3258988e94607863051d57bf48e09f5b65c4bec7
spencerm89 <smaynes89@gmail.com>
2014-05-19 15:27:22 -0500

    Update derived.2.r

--- R/derived.2.r
+++ R/derived.2.r
@@ -2,6 +2,7 @@ derived.2 <- function(b.x,b.y,retention,period) {
 area<-abs(pi*b.x*retention)
 coercion <- b.x/(sqrt(1+(b.y/retention)^2))
 lag<-abs(atan2(retention,b.y))*period/(pi*2)
+if (lag > period) lag <- NA
 z <- c(area,lag,coercion)
 z
 }
