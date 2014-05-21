8d9f906e6a59a9c78c0558b13f7ccf5436d71055
spencerm89 <smaynes89@gmail.com>
2014-05-21 11:24:07 -0500

    Update plot.loopsummary.R

--- R/plot.loopsummary.R
+++ R/plot.loopsummary.R
@@ -16,8 +16,8 @@ plot.loopsummary <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FA
     Output <- a$values["cy","Boot.Estimate"]+direcsin*a$values["retention","Boot.Estimate"]*abs(sintp)^a$values["m","Boot.Estimate"]+direc*a$values["b.y","Boot.Estimate"]*abs(costp)^a$values["n","Boot.Estimate"]
     
   }
-    if (a$extended.classical==FALSE & a$method=="harmonic2") split.line <- a$values["b.y","Boot.Estimate"]*cos(ti)^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
-  else  split.line <- sign(cos(ti))*a$values["b.y","Boot.Estimate"]*abs(cos(ti))^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
+    if (a$extended.classical==FALSE & a$method=="harmonic2") splitLine <- a$values["b.y","Boot.Estimate"]*cos(ti)^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
+  else  splitLine <- sign(cos(ti))*a$values["b.y","Boot.Estimate"]*abs(cos(ti))^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
 
   if (is.null(xlim)) xlim <-c(min(c(a$x,Input)),max(c(a$x,Input)))
   if (is.null(ylim)) ylim <- c(min(c(a$y,Output,split.line)),max(c(a$y,Output,split.line)))                           
@@ -68,6 +68,6 @@ plot.loopsummary <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FA
   points(a$y~a$x,pch=1,cex=0.85)
   if (split.line==TRUE) {
 
-  lines(Input,split.line,lty=2)}
+  lines(Input,splitLine,lty=2)}
   if(putNumber==TRUE) text(a$x,a$y,as.character(format(1:length(a$y),digits=4)))
 }
