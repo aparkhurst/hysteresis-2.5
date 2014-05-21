62700e2bc20c7e95f1878d21e556de7ab02e7d4e
spencerm89 <smaynes89@gmail.com>
2014-05-21 11:21:23 -0500

    Update plot.fittedloop.R

--- R/plot.fittedloop.R
+++ R/plot.fittedloop.R
@@ -16,8 +16,8 @@ plot.fittedloop <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FAL
     Output <- a$values["cy"]+direcsin*a$values["retention"]*abs(sintp)^a$values["m"]+direc*a$values["b.y"]*abs(costp)^a$values["n"]
     
   }
-   if (a$extended.classical==FALSE & a$method=="harmonic2") split.line <- a$values["b.y"]*cos(ti)^a$values["n"]+a$values["cy"]
-  else split.line <- sign(cos(ti))*a$values["b.y"]*abs(cos(ti))^a$values["n"]+a$values["cy"]
+   if (a$extended.classical==FALSE & a$method=="harmonic2") splitLine <- a$values["b.y"]*cos(ti)^a$values["n"]+a$values["cy"]
+  else splitLine <- sign(cos(ti))*a$values["b.y"]*abs(cos(ti))^a$values["n"]+a$values["cy"]
   if (is.null(xlim)) xlim <-c(min(c(a$x,Input)),max(c(a$x,Input)))
   if (is.null(ylim)) ylim <- c(min(c(a$y,Output,split.line)),max(c(a$y,Output,split.line)))                           
  if (is.null(values)) plot(Output~Input,type="l",ylim=ylim,xlim=xlim,main=main,...)
@@ -65,6 +65,6 @@ plot.fittedloop <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FAL
   
   points(a$y~a$x,pch=1,cex=0.85)
   if (split.line==TRUE) {
-  lines(Input,split.line,lty=2)}
+  lines(Input,splitLine,lty=2)}
   if(putNumber==TRUE) text(a$x,a$y,as.character(format(1:length(a$y),digits=4)))
 }
