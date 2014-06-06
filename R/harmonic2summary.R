3889efceded6777bed70d7634011e74ee93b4339
spencerm89 <smaynes89@gmail.com>
2014-06-06 14:16:23 -0500

    Update harmonic2summary.R

--- R/harmonic2summary.R
+++ R/harmonic2summary.R
@@ -81,7 +81,7 @@ if (studentize==TRUE) {
 }
 bmcoefs <- matrix(NA,nrow=j,ncol=j+1)
 for (j in 1:dim(biasmat)[1]) {
-biasmodel <- lars(x=cbind(rep(1,200),t(themeanmat)),y=t(biasmat[j,])) 
+biasmodel <- lars(x=cbind(rep(1,200),t(themeanmat)),y=as.vector(biasmat[j,])) 
 try(bmcoefs[j,] <- coef(biasmodel)[which.min(summary(biasmodel)$Cp),])
 }
 bmcoefs[is.na(bmcoefs)] <- 0
