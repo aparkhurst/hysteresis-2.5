892ea66521173d6f4479a2886467b48155b0cff5
spencerm89 <smaynes89@gmail.com>
2014-06-14 15:56:03 -0500

    Update plot.fittedloop.R

--- R/plot.fittedloop.R
+++ R/plot.fittedloop.R
@@ -18,8 +18,8 @@ plot.fittedloop <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=FAL
   }
    if (a$extended.classical==FALSE & a$method=="harmonic2") splitLine <- a$values["b.y"]*cos(ti)^a$values["n"]+a$values["cy"]
   else splitLine <- sign(cos(ti))*a$values["b.y"]*abs(cos(ti))^a$values["n"]+a$values["cy"]
-  if (is.null(xlim)) xlim <-c(min(c(a$x,Input)),max(c(a$x,Input)))
-  if (is.null(ylim)) ylim <- c(min(c(a$y,Output,split.line)),max(c(a$y,Output,split.line)))                           
+  if (is.null(xlim)) xlim <-range(c(a$x,Input))
+  if (is.null(ylim)) ylim <- range(c(a$y,Output,splitLine))                           
  if (is.null(values)) plot(Output~Input,type="l",ylim=ylim,xlim=xlim,main=main,...)
   else {
     if (values=="inherent") {
