ad76c5c3e0cad10bf05958870ead9a2b61b9ab6a
spencerm89 <smaynes89@gmail.com>
2014-06-14 16:09:03 -0500

    Update plot.loop2rsummary.R

--- R/plot.loop2rsummary.R
+++ R/plot.loop2rsummary.R
@@ -17,11 +17,11 @@ plot.loop2rsummary <- function (x,split.line=TRUE,xlim=NULL,ylim=NULL,putNumber=
     Output <- a$values["cy","Boot.Estimate"]+(direcsin < 0)*direcsin*a$values["retention.below","Boot.Estimate"]*abs(sintp)^a$values["m","Boot.Estimate"]+(direcsin > 0)*direcsin*a$values["retention.above","Boot.Estimate"]*abs(sintp)^a$values["m","Boot.Estimate"]+direc*a$values["b.y","Boot.Estimate"]*abs(costp)^a$values["n","Boot.Estimate"]
     
   }
-  if (a$extended.classical==FALSE & a$method=="harmonic2") split.line <- a$values["b.y","Boot.Estimate"]*cos(ti)^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
-  else  split.line <- sign(cos(ti))*a$values["b.y","Boot.Estimate"]*abs(cos(ti))^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
+  if (a$extended.classical==FALSE & a$method=="harmonic2") splitLine <- a$values["b.y","Boot.Estimate"]*cos(ti)^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
+  else  splitLine <- sign(cos(ti))*a$values["b.y","Boot.Estimate"]*abs(cos(ti))^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
   
-  if (is.null(xlim)) xlim <-c(min(c(a$x,Input)),max(c(a$x,Input)))
-  if (is.null(ylim)) ylim <- c(min(c(a$y,Output,split.line)),max(c(a$y,Output,split.line)))                           
+  if (is.null(xlim)) xlim <-range(c(a$x,Input))
+  if (is.null(ylim)) ylim <- range(c(a$y,Output,split.line))                           
 if (is.null(values)) plot(Output~Input,type="l",ylim=ylim,xlim=xlim,main=main,...)
   else {
     if (values=="inherent") {
@@ -55,8 +55,6 @@ if (is.null(values)) plot(Output~Input,type="l",ylim=ylim,xlim=xlim,main=main,..
   
   points(a$y~a$x,pch=1,cex=0.85)
   if (split.line==TRUE) {
-  if (a$extended.classical==FALSE & a$method=="harmonic2") splitLine <- a$values["b.y","Boot.Estimate"]*cos(ti)^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
-  else  splitLine <- sign(cos(ti))*a$values["b.y","Boot.Estimate"]*abs(cos(ti))^a$values["n","Boot.Estimate"]+a$values["cy","Boot.Estimate"]
-  lines(Input,splitLine,lty=2)}
+   lines(Input,splitLine,lty=2)}
   if(putNumber==TRUE) text(a$x,a$y,as.character(format(1:length(a$y),digits=4)))
 }
