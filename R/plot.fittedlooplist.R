0b79be21e94649de2f344a6df7f02d48ea90c2b6
spencerm89 <smaynes89@gmail.com>
2014-07-03 10:20:17 -0500

    Update plot.fittedlooplist.R

--- R/plot.fittedlooplist.R
+++ R/plot.fittedlooplist.R
@@ -11,11 +11,11 @@ else {
 
 plot.fittedlooplist2r<-function(x,main=NULL,values=NULL,...){
   a <- x
-   if (length(dim(a$models))<=1) mapply(plot.fittedloop2r,a$models,main=names(a$models),MoreArgs=list(values=values,...))
+   if (length(dim(a$models))<=1) mapply(plot.loop2r,a$models,main=names(a$models),MoreArgs=list(values=values,...))
 else {
   length.values <- length(a$models[[1]]$values)
   thenames <- do.call(paste,lapply(seq_len(ncol(a$Estimates)-length.values), function(i) a$Estimates[,i]))
-  mapply(plot.fittedloop2r,a$models,main=thenames,MoreArgs=list(values=values,...))
+  mapply(plot.loop2r,a$models,main=thenames,MoreArgs=list(values=values,...))
 }
   if (!is.null(main)) mtext(paste(main),side=3,outer=T)
   }
