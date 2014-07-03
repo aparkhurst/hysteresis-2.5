4cd6ce5ea3a6d0bf7c5b7a14d547056d8bb60485
spencerm89 <smaynes89@gmail.com>
2014-07-03 10:19:12 -0500

    Update plot.loopsummarylist.R

--- R/plot.loopsummarylist.R
+++ R/plot.loopsummarylist.R
@@ -8,3 +8,14 @@ plot.loopsummarylist <- function(x,main=NULL,values=NULL,...){
   }
   if (!is.null(main)) mtext(paste(main),side=3,outer=T)
 }
+
+plot.loopsummarylist2r <- function(x,main=NULL,values=NULL,...){
+  a <- x
+  if (length(dim(a$models))<=1) mapply(plot.loopsummary2r,a$models,main=names(a$models),MoreArgs=list(values=values,...))
+  else {
+    length.values <- length(a$models[[1]]$values[,1])
+    thenames <- do.call(paste,lapply(seq_len(ncol(a$Boot.Estimates)-length.values), function(i) a$Boot.Estimates[,i]))
+    mapply(plot.loopsummary2r,a$models,main=thenames,MoreArgs=list(values=values,...))
+  }
+  if (!is.null(main)) mtext(paste(main),side=3,outer=T)
+}
