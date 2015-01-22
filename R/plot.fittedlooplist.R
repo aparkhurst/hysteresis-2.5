2070589dfa3bfbbb5b5cb41d346b58a60e5c2d7f
spencerm89 <smaynes89@gmail.com>
2015-01-21 21:26:14 -0600

    Update plot.fittedlooplist.R

--- R/plot.fittedlooplist.R
+++ R/plot.fittedlooplist.R
@@ -2,7 +2,7 @@ plot.fittedlooplist<-function(x,main=NULL,values=NULL,...){
   a <- x
    if (length(dim(a$models))<=1) mapply(plot.fittedloop,a$models,main=names(a$models),MoreArgs=list(values=values,...))
 else {
-  length.values <- length(a$models[[1]]$values)-any(names(a$models[[1]]$values)=="n")
+  length.values <- length(a$models[[1]]$values)
   thenames <- do.call(paste,lapply(seq_len(ncol(a$Estimates)-length.values), function(i) a$Estimates[,i]))
   mapply(plot.fittedloop,a$models,main=thenames,MoreArgs=list(values=values,...))
 }
@@ -13,7 +13,7 @@ plot.fittedlooplist2r<-function(x,main=NULL,values=NULL,...){
   a <- x
    if (length(dim(a$models))<=1) mapply(plot.loop2r,a$models,main=names(a$models),MoreArgs=list(values=values,...))
 else {
-  length.values <- length(a$models[[1]]$values)-any(names(a$models[[1]]$values)=="n")
+  length.values <- length(a$models[[1]]$values)
   thenames <- do.call(paste,lapply(seq_len(ncol(a$Estimates)-length.values), function(i) a$Estimates[,i]))
   mapply(plot.loop2r,a$models,main=thenames,MoreArgs=list(values=values,...))
 }
